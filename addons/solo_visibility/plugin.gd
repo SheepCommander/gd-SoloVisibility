a6960c0531e8bbda587131ad04416def36a800aa
SheepCommander <66646514+SheepCommander@users.noreply.github.com>
2024-05-22 00:17:39 -0700

    v1.1 fixed Hotkey activation issue, fixed scope of action
    1) It was previously possible for hotkeys to activate while inside the
    Script Editor.
    
    This has been removed, however Godot requires you to click inside the 2D
    or 3D viewport before it allows you to click hotkeys.
    A workaround is in progress.
    2) The Action in the Editor's UndoRedo history was previously Global.
    Now, it is per-Scene and works much better.

--- addons/solo_visibility/plugin.gd
+++ addons/solo_visibility/plugin.gd
@@ -4,40 +4,67 @@ extends EditorPlugin
 ## 
 ## Shift + H to hide all nodes except selection & children
 
-var HideShortcut: InputEventKey = InputEventKey.new()
+var HideShortcut: InputEventKey
 
 
+# Virtual functions #
 func _enter_tree() -> void:
 	HideShortcut = preload("res://addons/solo_visibility/hide_nodes_shortcut.tres")
 	print("Solo Visibility - Loaded shortcuts")
 
 
-func _input(event: InputEvent) -> void:
+func _handles(object: Object) -> bool:
+	return true # Tells Godot to pass inputs to the below methods.
+
+
+func _forward_3d_gui_input(viewport_camera: Camera3D, event: InputEvent) -> int:
 	if (HideShortcut.is_match(event) and event.is_pressed() and !event.is_echo()):
 		commit_hide_nodes()
+	
+	return EditorPlugin.AFTER_GUI_INPUT_PASS
 
 
-## Hide ##
-func commit_hide_nodes():
-	var undo_redo = get_undo_redo()
+func _forward_canvas_gui_input(event: InputEvent) -> bool:
+	if (HideShortcut.is_match(event) and event.is_pressed() and !event.is_echo()):
+		commit_hide_nodes()
+	
+	return EditorPlugin.AFTER_GUI_INPUT_PASS
+
+
+func _exit_tree() -> void:
+	NextCacheID = 0
+	cache.clear()
+
+
+# Hiding functions #
+func commit_hide_nodes() -> void:
+	var undo_redo := get_undo_redo()
 	
 	var selected = get_editor_interface().get_selection().get_transformable_selected_nodes()
-	var root_node = get_editor_interface().get_edited_scene_root().get_children()
+	var root = get_editor_interface().get_edited_scene_root()
+	var root_nodes = root.get_children()
 	var currCashID = NextCacheID
-
-	undo_redo.create_action("Hide Non-Selected Nodes")
-	undo_redo.add_do_method(self, "hide", root_node, selected, false, currCashID)
-	undo_redo.add_undo_method(self, "hide", root_node, selected, true, currCashID)
-	undo_redo.commit_action()
 	
+	# MERGE_ENDS seems to make it so if Shift+H is spammed, Undo only has to be pressed once.
+	# root is passed to make sure the UndoRedo context is Scene, rather than Global.
+	undo_redo.create_action("Hide Non-Selected Nodes", UndoRedo.MERGE_ENDS, root)
+	# Do
+	undo_redo.add_do_method(self, "hide", root_nodes, selected, false, currCashID)
+	# Undo
+	undo_redo.add_undo_method(self, "hide", root_nodes, selected, true, currCashID)
+	# Commit. Executions the do actions.
+	undo_redo.commit_action()
+	# Move to next cache slot
 	NextCacheID += 1
 
-static var cache : Dictionary = {
-	#0 : [Node.new(), Node.new()] ## What the dictionary should look like
-}
-static var NextCacheID : int = 0 ## Used to determine the key for `session`
 
-static func hide(nodes: Array[Node], excluding, undo, cacheID):
+var cache : Dictionary = { ## Keeps track of nodes that remain hidden after Undo is called
+	#0 : [node, node2] ## Example of what the dictionary should look like
+}
+var NextCacheID : int = 0 ## Used to determine the key for `cache`
+
+
+func hide(nodes: Array[Node], excluding, undo, cacheID) -> void:
 	var queue := nodes.duplicate()
 	if not undo:
 		cache[cacheID] = []
@@ -48,7 +75,7 @@ static func hide(nodes: Array[Node], excluding, undo, cacheID):
 			continue # Skip iterating over any of the excluded nodes, or their children
 		
 		if node.has_method("hide"): # Only hide nodes w/ the is_hide property
-			if node.visible == false and not undo: # If the node was already hidden before hide() called
+			if not undo and node.visible == false: # If the node was already hidden before hide() called
 				cache[cacheID].append(node)
 			node.visible = undo if not node in cache[cacheID] else false
 		
@@ -57,8 +84,3 @@ static func hide(nodes: Array[Node], excluding, undo, cacheID):
 	
 	if undo:
 		cache[cacheID].clear()
-
-
-func _exit_tree() -> void:
-	NextCacheID = 0
-	cache.clear()
