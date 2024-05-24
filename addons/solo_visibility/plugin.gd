4353a458d518ebd2a72cc64d887dedc02300fbae
ShepChromebook <93790365+ShepChromebook@users.noreply.github.com>
2024-05-24 12:39:22 -0700

    v3 Issue #3 is resolved! But not Issue #2...

--- addons/solo_visibility/plugin.gd
+++ addons/solo_visibility/plugin.gd
@@ -1,184 +1,179 @@
 @tool
 extends EditorPlugin
 ## Solo Visibility
-## 
 ## [kbd]Shift + H[/kbd] to hide all nodes except selection & children
+## [kbd]Alt + H[/kbd] to reveal all nodes except selection & children
 
+const HIDE_SHORTCUT_PATH = "res://addons/solo_visibility/hide_nodes_shortcut.tres"
+const SHOW_SHORTCUT_PATH = "res://addons/solo_visibility/show_nodes_shortcut.tres"
+var HideShortcut: InputEventKey
+var ShowShortcut: InputEventKey
 
-## Set to true if the hotkey is activating inside the FileSystem and other unwanted docks.
-## If true, the hotkey will stop activating in the Scene dock and will only activate when the 2D or 3D viewports are focused.
-@export
-var Hotkey_Activates_When_I_Dont_Want_It_To: bool = false
-## Screens that the hotkey can be activated in.
-@export
-var AllowedScreens : Array[String] = [
-	"3D",
-	"2D",
-]
+const PLUGIN_CONFIG_PATH = "res://addons/solo_visibility/plugin.cfg"
+const PLUGIN_NAME = "Solo Visibility"
+var PluginConfig := ConfigFile.new()
+
+var AllowedScreens: Array[String] = ["3D","2D"]
+var AllowedFocuses : Array[String] = ["SceneTreeEditor", "Node3DEditorViewport", "CanvasItemEditorViewport"]
+var ConfigDictionary : Dictionary = {
+	"AllowedScreens": AllowedScreens,
+	"AllowedFocuses": AllowedFocuses,
+}
+
 var CurrentMainScreen: String
-var HideShortcut: InputEventKey ## Configurable via [code]solo_visibility/hide_nodes_shortcut.tres[/code]
-var ShowShortcut: InputEventKey ## @experimental SHOW SHORTCUT IS NOT IMPLEMENTED YET
 
-# --- Virtual functions --- #
+
 func _enter_tree() -> void:
-	HideShortcut = preload("res://addons/solo_visibility/hide_nodes_shortcut.tres")
-	ShowShortcut = preload("res://addons/solo_visibility/show_nodes_shortcut.tres")
-	print("Solo Visibility - Loaded shortcuts")
+	set_process_input(false) # Prevent startup "Cannot access <property> on null" errors
+	
+	HideShortcut = preload(HIDE_SHORTCUT_PATH)
+	ShowShortcut = preload(SHOW_SHORTCUT_PATH)
+	
+	PluginConfig.load(PLUGIN_CONFIG_PATH)
+	for key in ConfigDictionary:
+		if not PluginConfig.has_section_key(PLUGIN_NAME, key):
+			PluginConfig.set_value(PLUGIN_NAME, key, ConfigDictionary[key])
+		else:
+			if ConfigDictionary[key] is Array:
+				ConfigDictionary[key].assign(PluginConfig.get_value(PLUGIN_NAME, key))
+			else:
+				ConfigDictionary[key] = PluginConfig.get_value(PLUGIN_NAME, key)
+	PluginConfig.save(PLUGIN_CONFIG_PATH)
 	
 	main_screen_changed.connect(_on_main_screen_changed)
 
 
-func _input(event: InputEvent) -> void:
-	if Hotkey_Activates_When_I_Dont_Want_It_To: # Fallback to _shortcut_input if _input isnt working
-		set_process_input(false)
-		return
-	_solo_visibility_input(event)
-
-
-func _shortcut_input(event: InputEvent) -> void:
-	if Hotkey_Activates_When_I_Dont_Want_It_To: # Fallback to _shortcut_input if _input isnt working
-		_solo_visibility_input(event)
-
-
-func _solo_visibility_input(event: InputEvent):
-	if not event is InputEventKey:
-		return # Only proceed if the event is an InputEventKey (Does not proceed if InputEventMouseMotion, etc.)
-	
-	if not (event.is_pressed() and !event.is_echo()):
-		return # Only proceed if the event is_pressed() and not is_echo()	
-	
-	
-	if HideShortcut.is_match(event):
-		if CurrentMainScreen not in AllowedScreens:
-			print("Solo Visibility - Hotkey does not activate when %s screen is open" % CurrentMainScreen)
-			return
-		commit_hide_nodes()
-	
-	if ShowShortcut.is_match(event):
-		if CurrentMainScreen not in AllowedScreens:
-			print("Solo Visibility - Hotkey does not activate when %s screen is open" % CurrentMainScreen)
-			return
-		
+func _ready():
+	set_process_input(true) # Resume input after Godot has presumably finished startup
 
 
 func _exit_tree() -> void:
 	pass
 
 
-func _on_main_screen_changed(screen_name: String):
+func _on_main_screen_changed(screen_name: String) -> void:
 	CurrentMainScreen = screen_name
 
 
-# --- Code that handles hiding --- #
-## Used as the key for [member HiddenCache]
-var NextCacheID : int = 0
-## Each key registered corresponds to an [Array] of [Node]s that should stay hidden when [method _undo_hide] is called.
-var HiddenCache : Dictionary = {
-	#0: [Node, Node2] ## How the cache should look.
-}
-## Used by [method _] to get the [param dont_hide] used by the last [method commit_hide_nodes] call
-var DontHideCache : Dictionary = {
-	#0: [Node, Node2] ## How the cache should look.
-}
-## Use method [EditorUndoRedoManager.get_object_history_id(root_node)] to find a scene's unique ID / key.
-## The value corresponding to a key is an [Array] of [int] cacheIDs
-## 
-## Use [method Array.pop_last()] to find the most recent cacheID of a given scene.
-var SceneCache : Dictionary = {
-	## SceneID: [cacheID, cacheID, cacheID]
-	#2: [0, 1, 3]
-	#3: [2, 4]
-}
+func _input(event: InputEvent) -> void:
+	_solo_visibility_input(event)
 
 
-## Handles the execution of the Hide action, as well as registering it in the Editor's UndoRedo system.
-## Does nothing if no node is selected.
+func _solo_visibility_input(event: InputEvent) -> void:
+	if not event is InputEventKey:
+		return
+	if not (event.is_pressed() and !event.is_echo()):
+		return # Only proceed if the event is_pressed() and not is_echo()
+	
+	var focus = EditorInterface.get_base_control().get_window().gui_get_focus_owner()
+	if focus.get_class() not in AllowedFocuses and focus.get_parent().get_class() not in AllowedFocuses:
+		return # WARNING: Unstable if internal classes are renamed or focus is not (/a direct child of) the internal class
+	
+	match event.keycode:
+		HideShortcut.keycode:
+			if CurrentMainScreen not in AllowedScreens:
+				print("%s - Hotkey does not activate when %s screen is open" % [PLUGIN_NAME, CurrentMainScreen])
+				return
+			commit_hide_nodes()
+		ShowShortcut.keycode:
+			pass
+
+
+# ==== Implementation Section ==== #
+const HIDE_ACTION_NAME = "Hide Non-Selected Nodes"
+
+## Handles the Hide Non-Selecteds Nodes action.
+## Does nothing if no nodes are selected.
 func commit_hide_nodes() -> void:
 	var undo_redo := get_undo_redo()
 	
-	var root = get_editor_interface().get_edited_scene_root()
+	var root : Node = get_editor_interface().get_edited_scene_root()
 	var sceneID : int = undo_redo.get_object_history_id(root)
 	var cacheID : int = NextCacheID
 	NextCacheID += 1
 	
-	var selected = get_editor_interface().get_selection().get_transformable_selected_nodes()
-	if not selected:
-		return
+	var selected : Array[Node] = get_editor_interface().get_selection().get_transformable_selected_nodes()
+	if selected.is_empty():
+		return # There are no nodes selected
 	
-	undo_redo.create_action("Hide Non-Selected Nodes", UndoRedo.MERGE_ENDS, root)
+	undo_redo.create_action(HIDE_ACTION_NAME, UndoRedo.MERGE_ENDS, root) # Pass root for local context
 	undo_redo.add_do_method(self, "_do_hide", root, selected, cacheID, sceneID)
 	undo_redo.add_undo_method(self, "_undo_hide", root, selected, cacheID, sceneID)
 	undo_redo.commit_action()
 
 
-# Private method. Called by [method commit_hide_nodes]
-# 
-func _do_hide(hide: Node, dont_hide: Array[Node], cacheID: int, sceneID: int) -> void:
-	var queue : Array[Node] = hide.get_children()
+## Used as key for [member StayHiddenCache]
+var NextCacheID : int = 0
+## Stack (LIFO) of last-used CacheID in a given Scene. Use [method Array.pop_last]
+var SceneCacheIDs : Dictionary = {
+	#SceneID-int: [CacheID-int, CacheID-int]
+}
+## Cache of already-hidden nodes that should stay hidden on Undo Hide Non-Selected Nodes
+var StayHiddenCache : Dictionary = {
+	#CacheID-int: [AlreadyHiddenNode, AlreadyHiddenNode2]
+}
+## Cache [method _do_hide]'s [param dont_hide] for future use.
+var DontHideCache : Dictionary = {
+	#CacheID-int: [SelectedNode, SelectedNode2]
+}
 
-	HiddenCache[cacheID] = [] # Create array in the cache for first itme
+# Private method. Called by [method commit_hide_nodes]
+func _do_hide(root: Node, dont_hide: Array[Node], cacheID: int, sceneID: int) -> void:
+	# Initiate/Add arrays to cache
+	StayHiddenCache[cacheID] = []
 	DontHideCache[cacheID] = dont_hide.duplicate()
-
-	if SceneCache.has(sceneID): # If we already have a key-value pair for this SceneID, then append the new cacheID
-		SceneCache[sceneID].append(cacheID)
-	else: # Else, create array for first time
-		SceneCache[sceneID] = [cacheID]
 	
-	while queue.size() > 0:
-		var node : Node = queue.pop_front()
-		
-		if node in dont_hide: # Node is an excluded node, no need to check children
-			continue
-		
-		var ignore_node = false # Ignore Node if its a parent of an excluded node
-		for excluded_node : Node in dont_hide:
-			if node.find_child(excluded_node.name, true):
-				ignore_node = true
-				break
-		
-		if node.has_method("hide") and not ignore_node:
-			if node.visible == false: # If the node was already hidden before hide() called
-				HiddenCache[cacheID].append(node)
-			node.visible = false
-		
-		var children = node.get_children()
-		queue.append_array(children)
-
-
-# Private method. Called by [method commit_hide_nodes]
-# 
-func _undo_hide(hide: Node, dont_hide: Array, cacheID: int, sceneID: int):
-	var queue : Array[Node] = hide.get_children()
-
-	while queue.size() > 0:
-		var node : Node = queue.pop_front()
-		
-		if node in dont_hide: # Node is an excluded node, no need to check children
-			continue
-		
-		var ignore_node = false # Ignore Node if its a parent of an excluded node
-		for excluded_node : Node in dont_hide:
-			if node.find_child(excluded_node.name, true):
-				ignore_node = true
-				break
-		
-		if node.has_method("show") and not ignore_node:
-			if not node in HiddenCache[cacheID]:
-				node.visible = true
-		
-		var children = node.get_children()
-		queue.append_array(children)
+	if not SceneCacheIDs.has(sceneID):
+		SceneCacheIDs[sceneID] = [] # Initiate new cache under this scene if it doesnt exist
+	SceneCacheIDs[sceneID].append(cacheID) # Append cacheID to scene's list of cacheIDs
 	
-	# Clear the cache
-	HiddenCache[cacheID].clear()
-	SceneCache[sceneID].pop_back()
-
-
-# @experimental
-# Not implemented yet.
-func _do_undo_last_hide(root: Node):
+	# Handle hiding
 	var queue : Array[Node] = root.get_children()
 	
+	while queue.size() > 0:
+		var node : Node = queue.pop_front()
+		
+		if node in dont_hide: # Node is an excluded node, no need to check children
+			continue
+		
+		var ignore_node = false
+		for excluded_node : Node in dont_hide:
+			if node.find_child(excluded_node.name, true):
+				ignore_node = true
+				break
+		
+		if not ignore_node and node.has_method("hide"):
+			if node.visible == false: # Node is already hidden
+				StayHiddenCache[cacheID].append(node) # Append to cache
+			node.visible = false
+		
+		queue.append_array(node.get_children()) # Add node's children to the queue
+
+# Private method. Called by [method commit_hide_nodes]
+func _undo_hide(root: Node, dont_hide: Array[Node], cacheID: int, sceneID: int) -> void:
+	var queue : Array[Node] = root.get_children()
+	while queue.size() > 0:
+		var node : Node = queue.pop_front()
+		
+		if node in dont_hide: # Node was an excluded node, no need to check children
+			continue
+		
+		var ignore_node = false # Ignore Node if its a parent of an excluded node
+		for excluded_node : Node in dont_hide:
+			if node.find_child(excluded_node.name, true):
+				ignore_node = true
+				break
+		
+		if not ignore_node and node.has_method("show"):
+			if not node in StayHiddenCache[cacheID]:
+				node.visible = true # Only show node if it was not Hidden pre-Do Hide Non-Selected Nodes
+		
+		var children = node.get_children()
+		queue.append_array(children)
 	
-	
-	var ignore = DontHideCache # simulates [param dont_hide]
+	# Clear cache
+	DontHideCache[cacheID].clear()
+	StayHiddenCache[cacheID].clear()
+	# Remove this cacheID from the Scene's stack of cacheIDs.
+	var lastCacheID = SceneCacheIDs[sceneID].pop_back()
+	assert(cacheID == lastCacheID, "Somehow a previous '%s' was Undone instead of the most recent one!" % HIDE_ACTION_NAME)
