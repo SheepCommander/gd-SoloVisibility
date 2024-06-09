3878fc7d966a23d9b05d0727f7900553ef60bdf3
SheepCommander <66646514+SheepCommander@users.noreply.github.com>
2024-06-09 13:14:31 -0700

    FINALLY RESOLVE #2

--- addons/solo_visibility/plugin.gd
+++ addons/solo_visibility/plugin.gd
@@ -40,6 +40,11 @@ func _on_main_screen_changed(screen_name: String) -> void:
 	current_main_screen = screen_name
 
 
+func _exit_tree() -> void:
+	# TODO: Remove all metadata from nodes on exit
+	pass
+
+
 func _input(event: InputEvent) -> void:
 	if not event is InputEventKey: return
 	if not (event.is_pressed() and !event.is_echo()): return
@@ -53,23 +58,23 @@ func _input(event: InputEvent) -> void:
 		if focus.get_parent().get_class() not in AllowedFocuses:
 			return # UNSTABLE: internal name/hierarchy changes will break logic
 	
-	match event.keycode:
-		HideShortcut.keycode:
-			get_viewport().set_input_as_handled()
-			commit_hide_nodes()
-		ShowShortcut.keycode:
-			get_viewport().set_input_as_handled()
-			commit_show_nodes()
+	if HideShortcut.is_match(event, true):
+		get_viewport().set_input_as_handled()
+		commit_hide_nodes()
+	if ShowShortcut.is_match(event,true):
+		get_viewport().set_input_as_handled()
+		commit_show_nodes()
 
 
 # ==== Implementation Section ==== #
-const HIDE_ACTION_NAME = "Hide Non-Selected Nodes"
-const SHOW_ACTION_NAME = "Show Non-Selected Nodes"
-const STAY_HIDDEN : StringName = "SoloVisibility_StaysHidden"
+const HIDE_ACTION_NAME := "Hide Non-Selected Nodes"
+const SHOW_ACTION_NAME := "Show Non-Selected Nodes"
+const STAY_HIDDEN : StringName = "_SoloVis_Hide"
+const STAY_SHOWN : StringName = "_SoloVis_Show"
 var NextCacheID : int = 1
 
-## Handles the Hide Non-Selected Nodes action.
-## Does nothing if no nodes are selected.
+
+## Handles the Hide Non-Selected Nodes action. Returns if no nodes are selected.
 func commit_hide_nodes() -> void:
 	var selected : Array[Node] = get_editor_interface().get_selection().get_transformable_selected_nodes()
 	if selected.is_empty():
@@ -84,68 +89,78 @@ func commit_hide_nodes() -> void:
 	undo_redo.add_do_method(self, "_do_hide", root, selected, cacheID)
 	undo_redo.add_undo_method(self, "_undo_hide", root, selected, cacheID)
 	undo_redo.commit_action()
-## Handles the Show Non-Selected Nodes action.
-## Does nothing if no nodes are selected.
+
+
+func _do_hide(root: Node, selection: Array[Node], cacheID: int) -> void:
+	var hide := func(node: Node) -> void:
+			if node.visible == false:
+				node.set_meta(STAY_HIDDEN+str(cacheID), true)
+			node.visible = false
+	
+	_to_nonselected(hide, root, selection)
+
+
+func _undo_hide(root: Node, selection: Array[Node], cacheID: int) -> void:
+	var undo_hide := func(node: Node) -> void:
+			if not node.has_meta(STAY_HIDDEN+str(cacheID)):
+				node.visible = true
+			node.remove_meta(STAY_HIDDEN+str(cacheID))
+	
+	_to_nonselected(undo_hide, root, selection)
+
+
+# Calls [param nonselected] on all hideable, non-selected nodes.
+func _to_nonselected(nonselected: Callable, root: Node, selection: Array[Node]) -> void:
+	var queue : Array[Node] = root.get_children()
+	while queue.size() > 0:
+		var node : Node = queue.pop_back()
+		if node in selection:
+			continue # [node] is selected, do nothing
+		
+		var is_parent := false
+		for selected_node : Node in selection:
+			var selected_path := str(root.get_path_to(selected_node))
+			if selected_path.begins_with(str(root.get_path_to(node)))\
+						and not node.get_parent() == selected_node.get_parent():
+				is_parent = true # [node] has [selected_node] as a child.
+				break 
+		
+		if not is_parent and node.has_method("hide"):
+			nonselected.call(node)
+		
+		queue.append_array(node.get_children())
+
+
+## Handles the Show Non-Selected Nodes action. Returns if no nodes are selected.
 func commit_show_nodes() -> void:
 	var selected : Array[Node] = get_editor_interface().get_selection().get_transformable_selected_nodes()
 	if selected.is_empty():
 		return # There are no nodes selected
-	print("show")
+	
 	var root : Node = get_editor_interface().get_edited_scene_root()
 	var cacheID := NextCacheID
 	NextCacheID += 1
 	
 	var undo_redo : EditorUndoRedoManager = get_undo_redo()
 	undo_redo.create_action(SHOW_ACTION_NAME, UndoRedo.MERGE_DISABLE, root) # root for local context
-	undo_redo.add_do_method(self, "_undo_hide", root, selected, cacheID)
-	undo_redo.add_undo_method(self, "_do_hide", root, selected, cacheID)
+	undo_redo.add_do_method(self, "_do_show", root, selected, cacheID)
+	undo_redo.add_undo_method(self, "_undo_show", root, selected, cacheID)
 	undo_redo.commit_action()
 
 
-# Private method. Called by [method commit_hide_nodes]
-func _do_hide(root: Node, selection: Array[Node], cacheID: int) -> void:
-	var queue : Array[Node] = root.get_children()
-	while queue.size() > 0:
-		var node : Node = queue.pop_back()
-		if node in selection:
-			continue # Node is selected, dont hide
-		
-		var is_parent := false
-		for selected_node : Node in selection:
-			var selected_path := str(root.get_path_to(selected_node))
-			if selected_path.begins_with(str(root.get_path_to(node)))\
-						and not node.get_parent() == selected_node.get_parent():
-				is_parent = true # [node] has [selected_node] as a child.
-				break
-		
-		if not is_parent and node.has_method("hide"):
-			if node.visible == false:
-				node.set_meta(STAY_HIDDEN+str(cacheID), true)
-				#print("%s is prev hidden" % node)
-			node.visible = false
-		
-		queue.append_array(node.get_children())
+func _do_show(root: Node, selection: Array[Node], cacheID: int) -> void:
+	var do_show := func(node: Node) -> void:
+			if node.visible == true:
+				node.set_meta(STAY_SHOWN+str(cacheID), true)
+			node.visible = true
+	
+	_to_nonselected(do_show, root, selection)
 
 
-# Private method. Called by [method commit_hide_nodes]
-func _undo_hide(root: Node, selection: Array[Node], cacheID: int) -> void:
-	var queue : Array[Node] = root.get_children()
-	while queue.size() > 0:
-		var node : Node = queue.pop_back()
-		if node in selection:
-			continue # Node is selected, dont hide
-		
-		var is_parent := false
-		for selected_node : Node in selection:
-			var selected_path := str(root.get_path_to(selected_node))
-			if selected_path.begins_with(str(root.get_path_to(node)))\
-						and not node.get_parent() == selected_node.get_parent():
-				is_parent = true # [node] has [selected_node] as a child.
-				break
-		
-		if not is_parent and node.has_method("hide"):
-			if not node.has_meta(STAY_HIDDEN+str(cacheID)):
-				node.visible = true
-			node.remove_meta(STAY_HIDDEN+str(cacheID))
-		
-		queue.append_array(node.get_children())
+func _undo_show(root: Node, selection: Array[Node], cacheID: int) -> void:
+	var undo_show := func(node: Node) -> void:
+			if not node.has_meta(STAY_SHOWN+str(cacheID)):
+				node.visible = false
+			node.remove_meta(STAY_SHOWN+str(cacheID))
+	
+	_to_nonselected(undo_show, root, selection)
